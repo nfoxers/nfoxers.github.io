95e573450fef4f1ff39a10ce646a4faa115958c0
wkokplauo666 <narada.fox7pc@gmail.com>
2026-07-02 00:26:56 +0700

    added more untranslated stuff

---
+++ script/stuff/hd44780.js
@@ -0,0 +1,17 @@
+// todo: finish ts
+
+const rs = document.getElementById('rs');
+const rw = document.getElementById('rw');
+const e = document.getElementById('e');
+const o = document.getElementById('inst');
+
+document.querySelectorAll('.d').forEach(e => {
+  e.addEventListener('input', () => {
+    inst();
+  })
+})
+
+function inst() {
+  let t;
+
+}
